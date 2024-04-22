87759c0c9da986d4d541dab4c7dd312cb962e6e3
nohe <nohelly.patinoc@konradlorenz.edu.co>
2024-04-22 08:53:27 -0500

    Taller 2 Libro Zoe Henao Boza

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -2,4 +2,38 @@ package com.mycompany.libropoo;
 
 public class Libro {
     
-}
+    //Atributos
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int aniopubli;
+    private boolean leido=false;
+    
+    public Libro( String titulo, String autor, int aniopubli, boolean leido){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.aniopubli = aniopubli;
+        this.leido = leido;
+    }// Cierro clase Libro
+    
+    public String mostrarInformacion(){
+        return "Título: "+titulo+ ", Autor: "+ autor+ ", Año de ¨Publicación: "+aniopubli+", Género:"+genero;
+    }// Cierro metodo montrarInformacion
+    
+    public void marcarLeido(){
+        leido=true;
+    }// Cierro metodo marcarLeido
+
+    public boolean isLeido() {
+        return leido;
+    }
+
+    public boolean esAntiguo(){
+        if (aniopubli<1974){
+        return true;
+        }else{
+        return false;
+        }
+    }
+    
+}//Cierro Clase
